4204565669c25356f27c4e0575b9e6382319ad6f
Abhay2608 <abhaynanda94@gmail.com>
2019-01-17 10:51:34 +0530

    Added first file

---
+++ calc.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class calc{
+	public static void main(String[] args){
+		float a,b,res;
+		char choice,ch;		
+		Scanner scan = new Scanner(System.in);
+		
+		do{
+			System.out.print("1. ADD TWO Numbers\n");
+			System.out.print("2. Exit\n\n");
+			System.out.print("Enter choice: ");
+			choice = scan.next().charAt(0);
+			
+			switch(choice){
+				case '1':System.out.print("Enter two numbers");
+					 a = scan.nextFloat();
+					 b = scan.nextFloat();
+					 res = a + b;
+					 System.out.print("Result = " + res);
+					 break;
+
+				 case '2' :System.exit(0);
+						break;
+						
+				 default: System.out.print("Invalid choice");
+					  break;
+			}
+			System.out.print("\n-------------------------\n");
+		}while(choice !=2);					  				
+	}
+}
