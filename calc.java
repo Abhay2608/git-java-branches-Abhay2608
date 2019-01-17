3c9e4fd5550ef2cef4e3b6a66ce02569ba21a57e
Abhay2608 <abhaynanda94@gmail.com>
2019-01-17 19:25:17 +0530

    Added subtract module

--- calc.java
+++ calc.java
@@ -21,6 +21,12 @@ public class calc{
 					 res = a + b;
 					 System.out.print("Result = " + res);
 					 break;
+				case '3':System.out.print("Enter two numbers");
+					 a = scan.nextFloat();
+					 b = scan.nextFloat();
+					 res = a - b;
+					 System.out.print("Result = " + res);
+					 break;
 
 				 case '2' :System.exit(0);
 						break;
